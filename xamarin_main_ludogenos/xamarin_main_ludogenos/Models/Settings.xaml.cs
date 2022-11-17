8f5d584b53ebc498795a9d2b8a3f7dddd653199f
Clei21 <99683789+Clei21@users.noreply.github.com>
2022-11-17 20:02:33 -0300

    Finalização da interface, finalização no xaml e cs de todas as interfaces.

--- xamarin_main_ludogenos/xamarin_main_ludogenos/Models/Settings.xaml.cs
+++ xamarin_main_ludogenos/xamarin_main_ludogenos/Models/Settings.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +10,40 @@ namespace xamarin_main_ludogenos.Models
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Settings : ContentPage
     {
+
+        private string[] Strings = { "API", "Cancelar" };
+
+
+
+
+        private string MétodoOption;
+
         public Settings()
         {
             InitializeComponent();
+            MétodoText.Text = "API";
+
+        }
+
+        async void OnClickMétodo(object sender, EventArgs e)
+
+        {
+
+
+            string option = await DisplayActionSheet("Método de conexão", "cancelar", null, "API");
+            if (option != "cancelar" && option != null)
+            {
+                MétodoOption = option;
+                MétodoText.Text = option;
+            }
+        }
+        void BoldChanged(object sender, CheckedChangedEventArgs e)
+        {
+
+        }
+        void UnderlineChanged(object sender, CheckedChangedEventArgs e)
+        {
+
         }
     }
 }
